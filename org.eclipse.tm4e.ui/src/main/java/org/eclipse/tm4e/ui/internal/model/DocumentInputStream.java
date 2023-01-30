1a8caa58a276c1d82669ae1b263b5ad921ece77a
sebthom <sebthom@users.noreply.github.com>
2023-01-30 16:12:28 +0100

    implement DocumentInputStream#read(byte[],int,int)

--- org.eclipse.tm4e.ui/src/main/java/org/eclipse/tm4e/ui/internal/model/DocumentInputStream.java
+++ org.eclipse.tm4e.ui/src/main/java/org/eclipse/tm4e/ui/internal/model/DocumentInputStream.java
@@ -6,42 +6,72 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
- *     QNX Software System
+ * IBM Corporation - initial API and implementation
+ * QNX Software System
+ * Sebastian Thomschke - implement read(byte[], int, int)
  *******************************************************************************/
 package org.eclipse.tm4e.ui.internal.model;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
+
+
 /**
  * Input stream which reads from a document
  */
 public final class DocumentInputStream extends InputStream {
 
-	private final IDocument fDocument;
-	private int fCurrPos = 0;
+	private final IDocument doc;
+	private int pos = 0;
 
 	public DocumentInputStream(final IDocument document) {
-		fDocument= document;
+		doc = document;
 	}
 
 	public IDocument getDocument() {
-		return fDocument;
+		return doc;
+	}
+
+	@Override
+	public int read(@NonNullByDefault({}) final byte[] buff, final int buffOffset, final int len) throws IOException {
+		Objects.checkFromIndexSize(buffOffset, len, buff.length);
+
+		if (len == 0)
+			return 0;
+
+		final var docLen = doc.getLength();
+		if (pos >= docLen)
+			return -1;
+
+		var bytesRead = -1;
+		try {
+			buff[buffOffset] = (byte) doc.getChar(pos++);
+			bytesRead = 1;
+			while (bytesRead < len) {
+				if (pos >= docLen) {
+					break;
+				}
+				buff[buffOffset + bytesRead++] = (byte) doc.getChar(pos++);
+			}
+		} catch (final BadLocationException ex) {
+			// ignore
+		}
+		return bytesRead;
 	}
 
 	@Override
 	public int read() throws IOException {
-	 	try {
-		 	if (fCurrPos < fDocument.getLength()) {
-		 	 	return fDocument.getChar(fCurrPos++);
-		 	}
-	 	} catch (final BadLocationException e) {
-	 		// ignore
-	 	}
-	 	return -1;
+		try {
+			if (pos < doc.getLength())
+				return doc.getChar(pos++) & 0xFF;
+		} catch (final BadLocationException ex) {
+			// ignore
+		}
+		return -1;
 	}
-
 }
