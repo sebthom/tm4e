e1fa491751f4dfc846709bdffd85badc3c9924cf
sebthom <sebthom@users.noreply.github.com>
2023-03-14 13:45:29 +0100

    style: fix license header

--- org.eclipse.tm4e.ui/src/main/java/org/eclipse/tm4e/ui/themes/ThemeAssociation.java
+++ org.eclipse.tm4e.ui/src/main/java/org/eclipse/tm4e/ui/themes/ThemeAssociation.java
@@ -9,32 +9,10 @@
  * Contributors:
  * Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
  */
-/**
- * Copyright (c) 2015-2017 Angelo ZERR.
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- * Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
- */
 package org.eclipse.tm4e.ui.themes;
 
 import java.util.Objects;
 
-/**
- * Copyright (c) 2015-2017 Angelo ZERR.
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- * Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
- */
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.jdt.annotation.Nullable;
 
