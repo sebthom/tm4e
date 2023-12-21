3db6c981578746e2e9f3cbbdb3602b183b6a63bd
sebthom <sebthom@users.noreply.github.com>
2023-12-21 22:25:29 +0100

    refact: improve resource loading in TMResource

--- org.eclipse.tm4e.registry/src/main/java/org/eclipse/tm4e/registry/TMResource.java
+++ org.eclipse.tm4e.registry/src/main/java/org/eclipse/tm4e/registry/TMResource.java
@@ -11,12 +11,13 @@
  */
 package org.eclipse.tm4e.registry;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.jdt.annotation.Nullable;
@@ -65,24 +66,17 @@ public abstract class TMResource implements ITMResource {
 	@Override
 	@SuppressWarnings("resource")
 	public InputStream getInputStream() throws IOException {
-		return pluginId != null
+		return new BufferedInputStream(pluginId != null
 				? new URL(PLATFORM_PLUGIN + pluginId + "/" + path).openStream()
-				: new FileInputStream(new File(path));
+				: new FileInputStream(new File(path)));
 	}
 
 	protected @Nullable String getResourceContent() {
 		try (InputStream in = this.getInputStream()) {
-			return convertStreamToString(in);
+			return new String(in.readAllBytes(), StandardCharsets.UTF_8);
 		} catch (final Exception ex) {
-			ex.printStackTrace();
+			TMEclipseRegistryPlugin.logError(ex);
 		}
 		return null;
-	}
-
-	private static String convertStreamToString(final InputStream is) {
-		try (var s = new Scanner(is)) {
-			s.useDelimiter("\\A");
-			return s.hasNext() ? s.next() : "";
-		}
 	}
 }
